35a7599dfd7c43ab58478c810a7a62015de0ac47
timmoh-king <timothykariuki43@gmail.com>
2023-08-16 18:19:01 +0300

    auth context

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className='ml-7'>
+    <div>
       Welcome to M-Wallet
+      <Footer />
     </div>
   );
 }
