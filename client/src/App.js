9f9705905f9a57588cfdda60df7041237b2d3284
timmoh-king <timothykariuki43@gmail.com>
2023-08-16 13:36:26 +0300

    Input component

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,9 @@
 import React from 'react'
-import Input from './components/Input';
-import Button from './components/Button';
 
 function App() {
   return (
     <div className='ml-7'>
       Welcome to M-Wallet
-      <br />
-      <Input labelName="Email" placeHolder="Enter email address" inputStyle="w-[983px]" />
-      <Button buttonName="Get started" buttonStyle="text-white bg-navyBlue w-[120px] hover:bg-white" />
     </div>
   );
 }
