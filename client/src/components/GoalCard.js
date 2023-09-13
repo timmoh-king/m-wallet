6bfe01854a2047b2ca0e59d03e21f51b518d4ed2
timmoh-king <timothykariuki43@gmail.com>
2023-09-13 11:04:11 +0300

    frontend stk push changes

--- client/src/components/GoalCard.js
+++ client/src/components/GoalCard.js
@@ -29,7 +29,7 @@ const GoalCard = ({ goaltitle, date, targetAmt, savedAmt, walletId }) => {
                 'Authorization': `Bearer ${token}`
             }
         };
-      await axios.post("/api/stk/", { amount }, config);
+      await axios.post("http://localhost:3005/api/stk/", { amount }, config);
       const response = await axios.put(`http://localhost:3005/api/get_wallets/${walletId}`, { amount: amount }, config);
       setAmount(response.data.amount);
       clearForm()
